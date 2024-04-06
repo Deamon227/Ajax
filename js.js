3b520b260b20534a1e9b501a3a12c3b0de87dfa8
Deamon227 <deamonspades2000@gmail.com>
2024-04-06 11:05:41 +0700

    front end of the project

--- js.js
+++ js.js
@@ -3,12 +3,12 @@ function showList() {
     if (object == null){
         window.location.href = "file:///C:/Users/ADMIN/OneDrive/Desktop/VSC/login.html"
     }else{
-        let token = object.token;
+        // let token = object.token;
     $.ajax({
         headers:{
             'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            "Authorization": "Bearer " + token
+            'Content-Type': 'application/json'
+            // "Authorization": "Bearer " + token
         },
         type : "GET",
         url : "http://localhost:8080/api/type",
@@ -34,7 +34,7 @@ function createNewType(){
         if (object == null){
             window.location.href = "file:///C:/Users/ADMIN/OneDrive/Desktop/VSC/login.html"
         }else{
-            let token = object.token
+            // let token = object.token
         let name = document.getElementById("name").value;
             let newType = {
                 "name": name
@@ -43,8 +43,8 @@ function createNewType(){
             $.ajax({
                 headers:{
                     'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    "Authorization": "Bearer " + token
+                    'Content-Type': 'application/json'
+                    // "Authorization": "Bearer " + token
                 },
                 type: "POST",
                 data: JSON.stringify(newType),
@@ -60,14 +60,14 @@ function showFormEdit(id) {
     if (object == null){
         window.location.href = "file:///C:/Users/ADMIN/OneDrive/Desktop/VSC/login.html"
     }else{
-        let token = object.token;
+        // let token = object.token;
         document.getElementById("edit").style.display = "block";
         //goi ajax de show form
         $.ajax({
             headers:{
                 'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                "Authorization": "Bearer " + token
+                'Content-Type': 'application/json'
+                // "Authorization": "Bearer " + token
             },
             type : "GET",
             url : "http://localhost:8080/api/type/" + id,
@@ -85,22 +85,24 @@ function showFormEdit(id) {
 
 function saveEdit(){
     // b1 lay du lieu
+    event.preventDefault;
     let object =getHeaderToken();
     if (object == null){
         window.location.href = "file:///C:/Users/ADMIN/OneDrive/Desktop/VSC/login.html"
     }else{
-        let token = object.token;
+        // let token = object.token;
     let newname = document.getElementById("nameedit").value;
     let newid = document.getElementById("idedit").value;
     let editType = {
         "name": newname
     }
+    document.getElementById("edit").style.display = "none";
     // B2: goi AJAX
     $.ajax({
         headers:{
             'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            "Authorization": "Bearer " + token
+            'Content-Type': 'application/json'
+            // "Authorization": "Bearer " + token
         },
         type: "PUT",
         data: JSON.stringify(editType),
@@ -116,12 +118,12 @@ function deleteType(id){
     if (object == null){
         window.location.href = "file:///C:/Users/ADMIN/OneDrive/Desktop/VSC/login.html"
     }else{
-        let token = object.token;
+        // let token = object.token;
     $.ajax({
         headers:{
             'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            "Authorization": "Bearer " + token
+            'Content-Type': 'application/json'
+            // "Authorization": "Bearer " + token
         },
         type : "Delete",
         url : "http://localhost:8080/api/type/" + id,
@@ -144,8 +146,8 @@ function showTaskNo(){
     $.ajax({
         headers:{
             'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            "Authorization": "Bearer " + token
+            'Content-Type': 'application/json'
+            // "Authorization": "Bearer " + token
         },
         type : "GET",
         url : "http://localhost:8080/api/type/count",
@@ -169,30 +171,11 @@ function searchType(){
     $.ajax({
         headers:{
             'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            "Authorization": "Bearer " + token
+            'Content-Type': 'application/json'
+            // "Authorization": "Bearer " + token
         },
         type : "GET",
         url : "http://localhost:8080/api/type/search?key=" + search,
-        success : showListSearch
-    })
-}
-
-function showListSearch() {
-    let object = getHeaderToken();
-    let search = document.getElementById("search").value;
-    if (object == null){
-        window.location.href = "file:///C:/Users/ADMIN/OneDrive/Desktop/VSC/login.html"
-    }else{
-        let token = object.token;
-    $.ajax({
-        headers:{
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            "Authorization": "Bearer " + token
-        },
-        type : "GET",
-        url : "http://localhost:8080/api/search?key=" + search,
         success : function (data) {
             let noidung = "";
             for (let i = 0; i < data.length; i++) {
@@ -206,6 +189,89 @@ function showListSearch() {
             document.getElementById("content").innerHTML =noidung;
         }
     })
-    }
 }
-showListSearch();
+
+function countUser(){
+    $.ajax({
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+            // "Authorization": "Bearer " + token
+        },
+        type : "GET",
+        url : "http://localhost:8080/api/role",
+        success : function(data){
+            console.log(data);
+            let noidung = "";
+            for (let i = 0; i < data.length; i++) {
+                noidung+= `<div class="accordion-item">
+                <h2 class="accordion-header" id="headingOne">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapseOne" aria-expanded="true"
+                        aria-controls="collapseOne">
+                        ${data[i].name} has ${data[i].userNo} user(s)
+                    </button>
+                </h2>
+                <div id="collapseOne" class="accordion-collapse collapse show"
+                    aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                    <div class="accordion-body" id="content3">
+                    ${getUserByRole(data[i].id)}
+                    </div>
+                </div>
+            </div>`
+            }
+            document.getElementById("content2").innerHTML =noidung;
+        }
+    });
+}
+
+function getUserByRole(id){
+    let noidung = "";
+    $.ajax({
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+            // "Authorization": "Bearer " + token
+        },
+        type : "GET",
+        url : "http://localhost:8080/api/user/" + id,
+        success : function(data){
+            console.log(data);
+            for (let i = 0; i < data.length; i++) {
+                noidung+= `<p>${data[i].name}</p>`
+            }
+        }
+    })
+    return noidung;
+}
+// function showListSearch() {
+//     let object = getHeaderToken();
+//     let search = document.getElementById("search").value;
+//     if (object == null){
+//         window.location.href = "file:///C:/Users/ADMIN/OneDrive/Desktop/VSC/login.html"
+//     }else{
+//         // let token = object.token;
+//     $.ajax({
+//         headers:{
+//             'Accept': 'application/json',
+//             'Content-Type': 'application/json'
+//             // "Authorization": "Bearer " + token
+//         },
+//         type : "GET",
+//         url : "http://localhost:8080/api/search?key=" + search,
+//         success : function (data) {
+//             let noidung = "";
+//             for (let i = 0; i < data.length; i++) {
+//                 noidung+= `<tr>
+//              <td>${data[i].id}</td>
+//              <td>${data[i].name}</td>
+//              <td><p onclick = showFormEdit(${data[i].id})>update</p></td>
+//              <td><p onclick = deleteType(${data[i].id})>delete</p></td>
+//              </tr>`
+//             }
+//             document.getElementById("content").innerHTML =noidung;
+//         }
+//     })
+//     }
+// }
+// showListSearch();
--- onload.js
+++ onload.js
@@ -1,2 +1,3 @@
 showTaskNo();
 showList();
+countUser();
