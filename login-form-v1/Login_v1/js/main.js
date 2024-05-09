81a03eefe8bce55a968d97def7244fc0e749f88d
Deamon227 <deamonspades2000@gmail.com>
2024-05-09 18:08:43 +0700

    Add register

--- login-form-v1/Login_v1/js/main.js
+++ login-form-v1/Login_v1/js/main.js
@@ -70,15 +70,10 @@ function checkLogin() {
         type: "POST",
         data: JSON.stringify(user),
         url: "http://localhost:8080/api/auth/login",
-        try : {
             success: function(data){
                 console.log(data),
                 localStorage.setItem("object", JSON.stringify(data)),
                 window.location.href = "file:///C:/Users/ADMIN/OneDrive/Desktop/VSC/51680/site/contacts.html"
             }
-          },
-          catch(err) {
-            console.log(err);
-          }
     })
 }
